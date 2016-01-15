0e6471f7adb9026843fb63302f10956bca25b7f3
weppyk <weppyk@HNBRNO-AcerA8>
2016-01-15 13:49:13 +0100

    product entity setting ORM

--- src/AppBundle/Entity/Product.php
+++ src/AppBundle/Entity/Product.php
@@ -2,9 +2,35 @@
 //src/AppBundle/Entity/Product.php
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="product"
+ */
+
 class Product
 {
+    /**
+     * @ORM\Column(type="integer"
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @ORM\Column(type="string", Length=100)
+     */
     protected $name;
+    
+    /**
+     *
+     * @ORM\Column(type="decimal", scale=2)
+     */
     protected $price;
+    
+    /**
+     *
+     * @ORM\Column(type="text")
+     */
     protected $description;
 }
